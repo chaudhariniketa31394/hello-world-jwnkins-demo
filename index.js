c4fce5e214769d2d5ca88a6f5010bdc9426547be
Niketa <chaudhariniketa31392@gmail.com>
2020-09-25 10:54:18 -0700

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
   res.end(msg);
 });
+
+
+app.get("/url", (req, res, next) => {
+  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+ });
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
