426dc675e925bcd23f4ddba575479bda70e83c80
Niketa <chaudhariniketa31392@gmail.com>
2020-10-04 10:57:19 -0700

    chnages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ ssh ubuntu@18.134.130.160 <<EOF
     nvm install v12.16.1
     npm install
     npm install -g nodemon pm2
-    pm2 restart ecosystem.config.js
+    node restart ecosystem.config.js
     exit
 EOF
