cd4de3e73a4227d1d12b5c2561508898af6a9dc4
Niketa <chaudhariniketa31392@gmail.com>
2020-10-04 10:31:40 -0700

    chnages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 ssh ubuntu@18.134.130.160 <<EOF
-    cd ~/workspace/node-app-aws
+    cd ~/node-app-aws
     git pull origin master
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
     . ~/.nvm/nvm.sh
     nvm install v12.16.1
     npm install
     npm install -g nodemon pm2
-    pm2 restart ecosystem.config.js
+  
     exit
 EOF
